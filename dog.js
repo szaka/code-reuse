5dfb70ebb63aeec0d0325422f793ba2e1e27dfe4
JohnMichelin <jmichelinphone@gmail.com>
2019-01-30 08:49:52 -0800

    feat(initial): add starting state for dog.js

--- dog.js
+++ dog.js
@@ -1,1 +1,38 @@
+const dog = {
+  name: "Scout",
+  breed: ["Husky", "German Shepherd"],
+  age: 4,
+  happiness: 50,
+  hunger: 10,
+  energy: 100
+};
 
+dog["feed"] = function(food) {
+  if (dog.hunger - food > 0) {
+    dog.hunger -= food;
+  } else {
+    dog.hunger = 0;
+  }
+};
+
+dog["play"] = function(time) {
+  if (dog.happiness + time < 100) {
+    dog.happiness += time;
+  } else {
+    dog.happiness = 100;
+  }
+
+  if (dog.energy - time > 0) {
+    dog.energy -= time;
+  } else {
+    dog.energy = 0;
+  }
+};
+
+dog["nap"] = function(time) {
+  if (dog.energy + time < 100) {
+    dog.energy += time;
+  } else {
+    dog.energy = 100;
+  }
+};
